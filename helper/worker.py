35c4b0a172d534b408d2c370bfb6c43d6917834d
lolmanmm <84611952+lolmanmm@users.noreply.github.com>
2021-06-04 14:33:45 +0530

    Paarth[DARK]
    Added new codes for ffmpeg

--- helper/worker.py
+++ helper/worker.py
@@ -79,7 +79,7 @@ async def encc(e):
                 [Button.inline("CANCEL PROCESS", data=f"skip{wah}")],
             ],
         )
-        cmd = f'ffmpeg -i "{dl}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{out}" -y'
+        cmd = f'ffmpeg -i "{dl}" -preset medium -c:v libx265 -crf 29 -map 0:v -map 0:a -c:s copy -map 0:s? -pix_fmt yuv420p10 -c:a libfdk_aac -profile:a aac_he_v2 "{out}" -y'
         process = await asyncio.create_subprocess_shell(
             cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
         )
