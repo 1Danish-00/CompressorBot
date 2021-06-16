608bc7c94b6322f69262763fba914b10b815cae0
Disneyteam <85325455+Disneyteam76@users.noreply.github.com>
2021-06-16 12:37:14 +0530

    Update

--- helper/funcn.py
+++ helper/funcn.py
@@ -17,7 +17,7 @@ from .config import *
 COUNT = []
 
 uptime = dt.now()
-os.system("wget https://telegra.ph/file/75ee20ec8d8c8bba84f02.jpg -O thumb.jpg")
+os.system("wget https://telegra.ph/file/4577cf43c6e966bf92bd6.jpg -O thumb.jpg")
 
 if not os.path.isdir("downloads/"):
     os.mkdir("downloads/")
