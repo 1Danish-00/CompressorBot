f221b40d87294b3b873d11052f4233bbce8073ff
lolmanmm <84611952+lolmanmm@users.noreply.github.com>
2021-07-09 18:30:25 +0530

    Update worker.py

--- helper/worker.py
+++ helper/worker.py
@@ -79,7 +79,7 @@ async def encc(e):
                 [Button.inline("CANCEL PROCESS", data=f"skip{wah}")],
             ],
         )
-        cmd = f'ffmpeg -i "{dl}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{out}" -y'
+        cmd = f'ffmpeg -i "{dl}" -preset veryfast -c:v libx264 -crf 32 -b:v 200k -map 0:v -c:a libopus -b:a 40k -map 0:a -c:s copy -map 0:s? "{out}" -y'
         process = await asyncio.create_subprocess_shell(
             cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
         )
