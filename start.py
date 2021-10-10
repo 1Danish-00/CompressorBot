f093fbf4091f0cecfd38a78b507c98bb2109b9a9
salomovasliddin3004 <85295223+salomovasliddin3004@users.noreply.github.com>
2021-10-10 14:28:24 +0500

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 #    This file is part of the Compressor distribution.
-#    Copyright (c) 2021 Danish_00
+#    Copyright (c) 2021 Danish_000
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
