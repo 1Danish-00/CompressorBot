8df6d07dfafdf96a0aad0482702d387b994b5db2
kucbkto <95414625+kucbkto@users.noreply.github.com>
2022-01-10 21:05:34 +0530

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -33,8 +33,8 @@ async def start(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/CompressorBot"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("MORE BOTS", url="https://t.me/kashir_bots"),
+                Button.url("DEVELOPER", url="t.me/kashmir_1"),
             ],
         ],
     )
@@ -60,8 +60,8 @@ async def beck(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("MORE BOTS", url="https://t.me/kashir_bots"),
+                Button.url("DEVELOPER", url="t.me/kashmir_1"),
             ],
         ],
     )
